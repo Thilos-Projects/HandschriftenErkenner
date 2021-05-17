ae11b6c75c21650751a81c38d51d8d6590047593
thre9100 <tfr@e-rat.de>
2021-05-17 13:50:45 +0200

    Eine Weitere Test

--- BildSpeicherer/bildSpeichererMain.py
+++ BildSpeicherer/bildSpeichererMain.py
@@ -1,11 +1,39 @@
 import sys
+import os
+import json
 from PIL import Image
 
-dictionarryName = input("von wem ist die schrift")
-isTest = input("ist datei für den Test J/N")
-dataPath = "F:\\Python\\InteligenteSysteme\\HandschriftenErkennung\\Bilder\\tom_handschrift.jpeg"
-targetPath = "../Bilder/"
+def createIndex():
+    if os.path.exists(indexPath):
+        return True
+    f = open(indexPath, "w")
+    return True
+    
+def doImage(path):
+    imageS0 = Image.open(path).resize((scaleHeight,int(scaleHeight*verhältnis)))
+    imageS0.save("F:\\Python\\InteligenteSysteme\\HandschriftenErkennung\\Bilder\\" + dictionarryName + "_" + path + ".jpg")
 
-image = Image.open(dataPath)
+def main():
+    dictionarryName = "Tom" #input("von wem ist die schrift")
+    #isTest = input("ist datei für den Test J/N")
+    dataPath = "F:\\Python\\InteligenteSysteme\\HandschriftenErkennung\\BildSpeicherer\\Input\\tom_handschrift.jpeg"
+    indexPath = "F:\\Python\\InteligenteSysteme\\HandschriftenErkennung\\TestDaten.json"
 
-print(image)
+    print(createIndex())
+
+    targetPath = "../Bilder/"
+
+    verhältnis = 3510.0/2550.0
+    scaleHeight = 300
+
+    if os.path.isdir(dataPath):
+        print("pfad")
+    elif os.path.isfile(dataPath):
+        doImage(path=dataPath)
+    else:
+        print("Nöööööö")
+
+
+
+if __name__=="__main__":
+    main()
